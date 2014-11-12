b2c83a998ff80cb1be36a727f4915d48f8989790
Amos Wenger <fasterthanlime@gmail.com>
2014-11-12 01:23:56 +0100

    Woops, forgot a few specs

--- spec/compiler_spec.rb
+++ spec/compiler_spec.rb
@@ -1,21 +1,21 @@
 
 RSpec.describe "Compiler" do
-  # it "tests output correctly" do
-  #   expect(%Q{
-  #          (print "Hello")
-  #          }).to have_output("Hello")
-  # end
+  it "tests output correctly" do
+    expect(%Q{
+           (print "Hello")
+           }).to have_output("Hello")
+  end
 
-  # it "tests output correctly (negative)" do
-  #   expect(%Q{}).not_to have_output("Hello")
-  # end
+  it "tests output correctly (negative)" do
+    expect(%Q{}).not_to have_output("Hello")
+  end
 
-  # it "defines and calls a simple function" do
-  #   expect(%Q{
-  #          (defn hello [] (print "Hello"))
-  #          (hello)
-  #          }).to have_output("Hello")
-  # end
+  it "defines and calls a simple function" do
+    expect(%Q{
+           (defn hello [] (print "Hello"))
+           (hello)
+           }).to have_output("Hello")
+  end
 
   it "provides list" do
     expect(%Q{
