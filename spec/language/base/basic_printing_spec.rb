a9d6464a2aa95c7e309e5b0b86a9f71d903a522b
Amos Wenger <fasterthanlime@gmail.com>
2014-11-13 15:43:34 +0100

    Spec++ (basic printing)

--- spec/language/base/basic_printing_spec.rb
+++ spec/language/base/basic_printing_spec.rb
@@ -8,11 +8,17 @@ RSpec.describe "Language", "basic printing" do
     expect(%Q{}).not_to have_output("Hello")
   end
 
-  it "tests output correctly (multiple)" do
+  it "tests output correctly (multiple calls)" do
     expect(%Q{
            (print "Hello")
            (print "dear")
            (print "world")
+           }).to have_output("Hello dear world")
+  end
+
+  it "tests output correctly (multiple args)" do
+    expect(%Q{
+           (print "Hello" "dear" "world")
            }).to have_output("Hello dear world")
   end
 end
