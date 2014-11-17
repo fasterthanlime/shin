788c59345811fda2463202a6d39e08d5beeeb6a0
Amos Wenger <fasterthanlime@gmail.com>
2014-11-17 15:46:23 +0100

    Keywords are callable! Symbols! Maps! Everything!

--- spec/language/collections/map_spec.rb
+++ spec/language/collections/map_spec.rb
@@ -4,6 +4,22 @@ RSpec.describe "Language", "map" do
     expect(%Q{ (print (get (hash-map (keyword "ruby") "Ruby" (keyword "cpp") "C++") (keyword "ruby"))) }).to have_output("Ruby")
   end
 
+  it "is callable with keywords" do
+    expect(%Q{ (print ({:ruby "Ruby" :cpp "C++"} :ruby)) }).to have_output("Ruby")
+  end
+
+  it "is callable with symbols" do
+    expect(%Q{ (print ({'ruby "Ruby" 'cpp "C++"} 'ruby)) }).to have_output("Ruby")
+  end
+
+  it "works with callable keywords" do
+    expect(%Q{ (print (:ruby {:ruby "Ruby" :cpp "C++"})) }).to have_output("Ruby")
+  end
+
+  it "works with callable symbols" do
+    expect(%Q{ (print ('ruby {'ruby "Ruby" 'cpp "C++"})) }).to have_output("Ruby")
+  end
+
   %w(map collection associative counted seqable).each do |property|
     it "satisfies #{property}?" do
       expect("(print (#{property}? (hash-map)))").to have_output("true")
---
+++ spec/language/interop/object_spec.rb
@@ -0,0 +1,13 @@
+
+RSpec.describe "Language", "object" do
+  it "has object literals and aget" do
+    expect(%Q{
+           (def dude {$ "name" "buddy holly"
+                        "job"  "singer"})
+           (print (aget dude "name") (aget job "job"))
+           }).to have_output("buddy holly singer")
+  end
+end
+
+
+
