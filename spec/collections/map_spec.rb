a35e27f1fe07fb9ff666a4daf2450d7a5fa5d7be
Amos Wenger <fasterthanlime@gmail.com>
2014-11-17 22:50:31 +0100

    map + spec

--- spec/collections/map_spec.rb
+++ spec/collections/map_spec.rb
@@ -1,29 +1,11 @@
 
 RSpec.describe "Language", "map" do
-  it "has working get" do
-    expect(%Q{ (print (get (hash-map (keyword "ruby") "Ruby" (keyword "cpp") "C++") (keyword "ruby"))) }).to have_output("Ruby")
+  it "works with vectors" do
+    expect(%Q{ (print (= [2 4 6 8] (map (fn [x] (* x 2)) [1 2 3 4]))) }).to have_output("true")
   end
 
-  it "is callable with keywords" do
-    expect(%Q{ (print ({:ruby "Ruby" :cpp "C++"} :ruby)) }).to have_output("Ruby")
-  end
-
-  it "is callable with symbols" do
-    expect(%Q{ (print ({'ruby "Ruby" 'cpp "C++"} 'ruby)) }).to have_output("Ruby")
-  end
-
-  it "works with callable keywords" do
-    expect(%Q{ (print (:ruby {:ruby "Ruby" :cpp "C++"})) }).to have_output("Ruby")
-  end
-
-  it "works with callable symbols" do
-    expect(%Q{ (print ('ruby {'ruby "Ruby" 'cpp "C++"})) }).to have_output("Ruby")
-  end
-
-  %w(map collection associative counted seqable).each do |property|
-    it "satisfies #{property}?" do
-      expect("(print (#{property}? (hash-map)))").to have_output("true")
-    end
+  it "works with lists" do
+    expect(%Q{ (print (= '(2 4 6 8) (map (fn [x] (* x 2)) '(1 2 3 4)))) }).to have_output("true")
   end
 end
 
