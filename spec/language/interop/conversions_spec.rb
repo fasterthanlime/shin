20bb1566c01dc6d36752597928baebea9cd3ec29
Amos Wenger <fasterthanlime@gmail.com>
2014-11-17 16:51:43 +0100

    js->clj

--- spec/language/interop/conversions_spec.rb
+++ spec/language/interop/conversions_spec.rb
@@ -21,6 +21,24 @@ RSpec.describe "Language", "conversions" do
       expect(%Q{ (print (aget (clj->js {"ruby" "Ruby" "cpp" "C++"}) "ruby")) }).to have_output("Ruby")
     end
   end
+
+  describe "js->cljs" do
+    it "works on strings" do
+      expect(%Q{ (print (js->clj "Hello")) }).to have_output("Hello")
+    end
+
+    it "works on numbers" do
+      expect(%Q{ (print (js->clj 42)) }).to have_output("42")
+    end
+
+    it "works on vectors" do
+      expect(%Q{ (print (get (js->clj [$ "one" "two" "three"]) 1)) }).to have_output("two")
+    end
+
+    it "works on maps" do
+      expect(%Q{ (print (get (js->clj {$ "ruby" "Ruby" "cpp" "C++"}) "ruby")) }).to have_output("Ruby")
+    end
+  end
 end
 
 
