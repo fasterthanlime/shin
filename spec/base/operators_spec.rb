c8192026677f2c6817aaca554d3dd34570f8aa8a
Amos Wenger <fasterthanlime@gmail.com>
2014-11-26 20:23:39 +0100

    Port more things to shin.

--- spec/base/operators_spec.rb
+++ spec/base/operators_spec.rb
@@ -1,5 +1,27 @@
 
 RSpec.describe "Language", "operators" do
+  it "has working truthy" do
+    expect('(print (truthy true))').to have_output("true")
+    expect('(print (truthy 1))').to have_output("true")
+    expect('(print (truthy "Hello"))').to have_output("true")
+    expect('(print (truthy {$}))').to have_output("true")
+    expect('(print (truthy [$]))').to have_output("true")
+    expect('(print (truthy nil))').to have_output("false")
+    expect('(print (truthy undefined))').to have_output("false")
+    expect('(print (truthy false))').to have_output("false")
+  end
+
+  it "has working falsey" do
+    expect('(print (falsey true))').to have_output("false")
+    expect('(print (falsey 1))').to have_output("false")
+    expect('(print (falsey "Hello"))').to have_output("false")
+    expect('(print (falsey {$}))').to have_output("false")
+    expect('(print (falsey [$]))').to have_output("false")
+    expect('(print (falsey nil))').to have_output("true")
+    expect('(print (falsey undefined))').to have_output("true")
+    expect('(print (falsey false))').to have_output("true")
+  end
+
   it "has working +" do
     expect("(print (+ 22 20))").to have_output("42")
     expect("(print (+ 11 30 2))").to have_output("43")
@@ -28,6 +50,15 @@ RSpec.describe "Language", "operators" do
     expect('(print (= (vector 1 2 3) (vector 1 2 3)))').to have_output("true")
     expect('(print (= 97 "banana"))').to have_output("false")
     expect('(print (= 97 (list 1 2 3)))').to have_output("false")
+  end
+
+  it "has working not=" do
+    expect('(print (not= 97 97))').to have_output("false")
+    expect('(print (not= "banana" "banana"))').to have_output("false")
+    expect('(print (not= (list 1 2 3) (list 1 2 3)))').to have_output("false")
+    expect('(print (not= (vector 1 2 3) (vector 1 2 3)))').to have_output("false")
+    expect('(print (not= 97 "banana"))').to have_output("true")
+    expect('(print (not= 97 (list 1 2 3)))').to have_output("true")
   end
 
   it "has working <=" do
