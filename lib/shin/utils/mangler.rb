0b16fb888a81f4ac96082e1274b83b68bae67cfe
Amos Wenger <fasterthanlime@gmail.com>
2014-11-27 04:19:51 +0100

    WOW. That is *much* faster.

--- lib/shin/utils/mangler.rb
+++ lib/shin/utils/mangler.rb
@@ -5,38 +5,34 @@ module Shin
       ID_START_REGEXP = /[A-Za-z\-_\*'\+\/\?!\$%&<>=\.\|]/
       ID_INNER_REGEXP = /[A-Za-z\-_\*'\+\/\?!\$%&<>=\.\|#\$0-9]/
 
+      MANGLE_REGEXP   = /[\-\*\+\/\?!\$%&<>=\.\|]/
+      UNMANGLE_REGEXP = /\$[\$_a-z]/
+
+      MANGLE_MAP = {
+        '-' => '$_',
+        '*' => '$m',
+        '+' => '$p',
+        '/' => '$v',
+        '?' => '$q',
+        '!' => '$e',
+        '$' => '$$',
+        '%' => '$c',
+        '&' => '$a',
+        '>' => '$g',
+        '<' => '$s',
+        '=' => '$l',
+        '.' => '$d',
+        '|' => '$i',
+      }
+      UNMANGLE_MAP = MANGLE_MAP.invert
+      raise "Mangle map has overlaps (#{MANGLE_MAP.count} vs #{UNMANGLE_MAP.count})" unless MANGLE_MAP.count == UNMANGLE_MAP.count
+
       def mangle(id)
-        id.
-          gsub('-', '$_').
-          gsub('?', '$q').
-          gsub('!', '$e').
-          gsub('*', '$m').
-          gsub('/', '$d').
-          gsub('+', '$p').
-          gsub('=', '$l').
-          gsub('>', '$g').
-          gsub('<', '$s').
-          gsub('.', '$d').
-          gsub('%', '$c').
-          gsub('|', '$p').
-          to_s
+        id.gsub(MANGLE_REGEXP, MANGLE_MAP)
       end
 
       def unmangle
-        id.
-          gsub('$_', '-').
-          gsub('$q', '?').
-          gsub('$e', '!').
-          gsub('$m', '*').
-          gsub('$d', '/').
-          gsub('$p', '+').
-          gsub('$l', '=').
-          gsub('$g', '>').
-          gsub('$s', '<').
-          gsub('$d', '.').
-          gsub('$c', '%').
-          gsub('$p', '|').
-          to_s
+        id.gsub(UNMANGLE_REGEXP, UNMANGLE_MAP)
       end
     end
   end
