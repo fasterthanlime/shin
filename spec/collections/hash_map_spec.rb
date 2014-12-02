53956db0d53d34f65cc14599a454e22c4740aac3
Amos Wenger <fasterthanlime@gmail.com>
2014-12-02 07:01:22 +0100

    Implement -seq on a map, with spec :shipit:

--- spec/collections/hash_map_spec.rb
+++ spec/collections/hash_map_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe "Language", "hash-map" do
     end
   end
 
+  it "has working seq" do
+    expect(%Q{
+           (print (reduce (fn [acc [k v]] (+ acc v)) 0 (seq {:a 1 :b 2 :c 3})))
+           }).to have_output("6")
+  end
+
   %w(map coll seqable associative counted reduceable).each do |property|
     it "satisfies #{property}?" do
       expect("(print (#{property}? {:a :Abaca}))").to have_output("true")
