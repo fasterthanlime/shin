a58bef61ba3c42e7b29ad0f552140f508be3baba
Amos Wenger <fasterthanlime@gmail.com>
2014-12-03 23:13:27 +0100

    Fast mutator proof of concept

--- lib/shin/fast_mutator.rb
+++ lib/shin/fast_mutator.rb
@@ -1,6 +1,11 @@
 
+require 'shin/utils/mangler'
+
 module Shin
   class FastMutator
+    include Shin::Utils::Mangler
+    include Shin::Utils::Mimic
+
     DEBUG = true
 
     def initialize(compiler, mod)
@@ -10,9 +15,6 @@ module Shin
 
     def expand(invoc, info, context)
       debug "Should expand #{invoc}"
-
-      args = invoc.inner.drop(1).to_a
-      debug "Got args: #{args.join(", ")}"
 
       deps = @compiler.collect_deps(info[:module])
       debug "Got deps: #{deps.keys}"
@@ -35,8 +37,46 @@ module Shin
       all_loaded = deps.keys.all? { |slug| context.spec_loaded?(context.parse_spec(slug)) }
       debug "All loaded? #{all_loaded}"
 
-      raise
-      invoc
+      macro_slug = info[:module].slug
+      debug "macro_slug: #{macro_slug}"
+
+      macro_name = invoc.inner.first.value
+      debug "macro_name: #{macro_name}"
+
+      macro_func = context.eval("$kir.modules['#{macro_slug}'].exports.#{mangle(macro_name)}")
+      unless macro_func
+        raise "Could not retrieve macro_func"
+      end
+      debug "macro_func: #{macro_func}"
+
+      macro_args = invoc.inner.drop(1).to_a
+      debug "macro_args: #{macro_args.join(", ")}"
+
+      macro_gifted_args = macro_args.map { |arg| unwrap(arg) }
+      debug "macro_gifted_args: #{macro_gifted_args.join(", ")}"
+
+      macro_ret = macro_func.call(*macro_gifted_args)
+      debug "macro_ret: #{macro_ret}"
+
+      macro_ret_unquoted = unquote(macro_ret, invoc.token)
+      debug "unquoted macro_ret: #{macro_ret_unquoted}"
+
+      macro_ret_unquoted
+    end
+
+    def unquote(node, token)
+      case node
+      when Shin::AST::Node
+        debug "TODO: dequote the inside of nodes"
+        # each, unquote, the works.
+        node
+      when Fixnum, Float, String
+        Shin::AST::Literal.new(token, node)
+      when V8::Object
+        type = v8_type(node)
+        debug "TODO: dequote a V8 object of type #{type}"
+        nil
+      end
     end
     
     private
--- spec/macros/fast_mutator_spec.rb
+++ spec/macros/fast_mutator_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "Language", "fast mutator" do
   it "tests the fast mutator" do
     expect(
       :source => %Q{ (print (fast-mutator-test "hello" " dolly")) },
-      :macros => %Q{ (defmacro fast-mutator-test [a b] `(str a b)) }
+      :macros => %Q{ (defmacro fast-mutator-test [a b] (str a b)) }
     ).to have_output("hello dolly")
   end
 end
