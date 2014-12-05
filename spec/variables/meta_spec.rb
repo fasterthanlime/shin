46047a58f4709602e1adf83af6bac211a7cc8d32
Amos Wenger <fasterthanlime@gmail.com>
2014-12-05 16:30:07 +0100

    Metadata on vectors #28

--- spec/variables/meta_spec.rb
+++ spec/variables/meta_spec.rb
@@ -2,11 +2,21 @@
 RSpec.describe "Language", "meta" do
   it "functions" do
     expect(%Q{
-           (let [fn (fn [])
-                 fm (with-meta fn {:key "Xzibit"})]
-             (print (nil? (meta fn)))
+           (let [f  (fn [])
+                 fm (with-meta f {:key "Xzibit"})]
+             (print (nil? (meta f)))
              (print (nil? (meta fm)))
              (print (:key (meta fm))))
+           }).to have_output(%w(true false Xzibit))
+  end
+
+  it "vectors" do
+    expect(%Q{
+           (let [v []
+                 vm (with-meta v {:key "Xzibit"})]
+             (print (nil? (meta v)))
+             (print (nil? (meta vm)))
+             (print (:key (meta vm))))
            }).to have_output(%w(true false Xzibit))
   end
 end
