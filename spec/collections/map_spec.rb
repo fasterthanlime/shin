e5a2fa0ae86cf8d3aca1929d3dac5d8f4223b280
Amos Wenger <fasterthanlime@gmail.com>
2014-12-10 02:29:54 +0100

    map-indexed

--- spec/collections/map_spec.rb
+++ spec/collections/map_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe "Language", "map" do
   it "works with lists" do
     expect(%Q{ (print (= '(2 4 6 8) (map (fn [x] (* x 2)) '(1 2 3 4)))) }).to have_output("true")
   end
+
+  describe "map-indexed" do
+    it "works" do
+      expect(%Q{ (print (pr-str (map-indexed (fn [i x] (* x i)) '(1 2 3 4)))) }).to have_output("(0 2 6 12)")
+    end
+  end
 end
 
 
